2774fc5045297006f9096079bcbbc4aef70bdd95
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2025-07-23 11:04:10 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-function mainClick() {
-    if (document.querySelector("#text").innerHTML === "") {
-        document.querySelector("#text").innerHTML = "Молодец!"
-        document.querySelector("#mainButton").innerHTML = "Вернуть"
-    }
-    else {
-        document.querySelector("#text").innerHTML = ""
-        document.querySelector("#mainButton").innerHTML = "Нажми"
-    }
+function back() {
+    window.location.href = "https://miroshkin0207.github.io/miroshkin"
 }
